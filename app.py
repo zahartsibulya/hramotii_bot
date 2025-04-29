6952f19975b4afb38eb0d9fb35ef03f01571e273
zahartsibulya <zahartsibulka@gmail.com>
2025-04-29 21:01:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def webhook():
         response_text = "На жаль, я ще не знаю точної відповіді, але вже вчуся!"
         with open("log.txt", "a", encoding="utf-8") as log_file:
             log_file.write(f"[{datetime.datetime.now()}] {user_query}\n")
-            print(f"Unknown question: {user_message}")
+            print(f"Unknown question: {response_text}")
     
     return jsonify({
         "fulfillmentText": response_text
